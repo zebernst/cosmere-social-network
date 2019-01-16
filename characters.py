e2edb6f3a4f633e89ed241fdfa2d4cbea9af4e62
Zach Bernstein <zbernstein@rightpoint.com>
2019-01-16 11:11:25 -0600

    restructure character dictionary into class
    reorganize utility methods to be encapsulated by character class

--- characters.py
+++ characters.py
@@ -1,3 +1,4 @@
+import operator
 import requests
 import json
 import re
@@ -6,131 +7,109 @@ from pathlib import Path
 from tqdm import tqdm
 
 
-def query():
-    """query coppermind.net api for all characters"""
-    # base code taken from https://www.mediawiki.org/wiki/API:Query#Continuing_queries
-    wiki_api = "https://coppermind.net/w/api.php"
+class Character:
+    """representation of a character in the Cosmere."""
+    def __init__(self, query_result: dict):
+        """construct character from coppermind.net api query results."""
+        self._discard = False
+        self._pageid = int(query_result['pageid'])
+        self._infobox_str = ""
 
-    # get total number of pages to fetch
-    r = requests.get(wiki_api, params=dict(action='query', format='json',
-                                           prop='categoryinfo', titles='Category:Characters'))
-    num_pages = r.json()['query']['pages']['40']['categoryinfo']['pages']
+        self.name = query_result['title']
+        self.info = self._parse_infobox(query_result)
+        self.aliases = self.info.pop('aliases', [])
+        self.titles = self.info.pop('titles', [])
+        self.world = self.info.pop('world', None)
+        self.books = self.info.pop('books', None)
 
-    # query server until finished
-    payload = {
-        "action": "query",
-        "format": "json",
-        "prop": "revisions",
-        "generator": "categorymembers",
-        "rvprop": "content",
-        "rvsection": "0",
-        "gcmtitle": "Category:Characters",
-        "gcmprop": "ids|title",
-        "gcmtype": "page",
-        "gcmlimit": "50",
-        "formatversion": 2
-    }
-    continue_data = {}
-    with tqdm(total=num_pages, unit=' pages') as progressbar:
-        while True:
-            req = payload.copy()
-            req.update(continue_data)
-            r = requests.get(wiki_api, params=req)
-            response = r.json()
-            if 'error' in response:
-                raise RuntimeError(response['error'])
-            if 'warnings' in response:
-                print(response['warnings'])
-            if 'query' in response:
-                yield response['query']['pages']
-            if 'continue' not in response:
-                progressbar.update(len(response['query']['pages']))
-                break
-
-            continue_data = response['continue']
-            progressbar.update(len(response['query']['pages']))
-
-
-def load_query_results():
-    """load and cache data from coppermind.net"""
-    path = Path('data/characters.json')
-    if path.exists():
-        with path.open() as f:
-            return json.load(f)
-    else:
-        print('downloading from coppermind.net...')
-        results = [el for batch in query() for el in batch]
-        with path.open('w') as f:
-            json.dump(results, f)
-        return results
-
-
-def isolate_table(s: str):
-    """isolate infobox from extraneous summary text in query results"""
-    count = 0
-    pos = 0
-    for i, char in enumerate(s):
-        if char == '{':
-            count += 1
-        elif char == '}':
-            count -= 1
-        if count == 0:
-            pos = i
-            break
-
-    return s[:pos+1] if pos else ""
-
-
-def sanitize_attrs(s: str):
-    """sanitize infobox key/value pairs"""
-    brace, bracket = 0, 0
-    sanitized = []
-    for char in s:
-        if char == '{':
-            brace += 1
-        elif char == '[':
-            bracket += 1
-        elif char == '}':
-            brace -= 1
-        elif char == ']':
-            bracket -= 1
-
-        # replace special chars with ':' if inside template entity
-        if char in ('|', '=') and (brace > 0 or bracket > 0):
-            sanitized.append(':')
+    def __eq__(self, other):
+        """return self == value."""
+        if isinstance(other, self.__class__):
+            return self._pageid == other._pageid
         else:
-            sanitized.append(char)
+            return False
 
-    return "".join(sanitized)
+    def __ne__(self, other):
+        """return self != value."""
+        return not self.__eq__(other)
 
+    def __hash__(self):
+        """return hash(self._pageid)."""
+        return hash(self._pageid)
 
-def process_data():
-    """get characters in the cosmere from coppermind.net"""
-    chars = []
-    for result in load_query_results():
+    def __repr__(self):
+        """return repr(self)."""
+        return f"<{self.name} ({self._pageid}): {self.world}>"
+
+    def __str__(self):
+        """return str(self)."""
+        return f"{self.name}"
+
+    @property
+    def monikers(self):
+        """return a set of monikers that the character is known by."""
+        return set(n for n in [self.name] + self.aliases + self.titles)
+
+    def _parse_infobox(self, query_result):
+        """..."""
 
         # set defaults
         char_info = {}
         table_str = ""
 
-        # parse infobox
-        if 'revisions' in result:
-            if result['revisions']:
-                content = result['revisions'][0]['content']
+        # define local utility functions
+        def isolate_table(s: str):
+            """isolate infobox from extraneous summary text in query results"""
+            count = 0
+            pos = 0
+            for i, char in enumerate(s):
+                if char == '{':
+                    count += 1
+                elif char == '}':
+                    count -= 1
+                if count == 0:
+                    pos = i
+                    break
 
-                # get infobox template content
-                s = isolate_table(content)
+            return s[:pos + 1] if pos else ""
+
+        def sanitize_templates(s: str):
+            """sanitize infobox key/value pairs"""
+            brace, bracket = 0, 0
+            sanitized = []
+            for char in s:
+                if char == '{':
+                    brace += 1
+                elif char == '[':
+                    bracket += 1
+                elif char == '}':
+                    brace -= 1
+                elif char == ']':
+                    bracket -= 1
+
+                # replace special chars with ':' if inside template entity
+                if char in ('|', '=') and (brace > 0 or bracket > 0):
+                    sanitized.append(':')
+                else:
+                    sanitized.append(char)
+
+            return "".join(sanitized)
+
+        # parse infobox
+        if 'revisions' in query_result:
+            if query_result['revisions']:
+                content = query_result['revisions'][0]['content']
 
                 # trim outermost template tags
                 match = re.match(r'^{{((?:.\s?)*)}}$', isolate_table(content))
                 if match:
                     # split string on field delimiter
-                    table_str = sanitize_attrs(match[1])
+                    table_str = sanitize_templates(match[1])
                     table = [e.strip() for e in table_str.split('|')]
 
                     # ignore non-character pages
                     if table.pop(0).lower() != 'character':
-                        continue
+                        self._discard = True
 
                     # split into key/value pairs
                     for entry in table:
@@ -141,7 +120,7 @@ def process_data():
                                 k, v = m[1], re.sub(r'[\[{}\]]', '', m[2])
 
                                 # ignore certain keywords
-                                if k.lower() in ('image', ):
+                                if k.lower() in ('image',):
                                     continue
 
                                 # sanitize and process specific fields
@@ -150,11 +129,12 @@ def process_data():
                                     v = [e.strip() for e in v.split(',')]
                                     v = [b.split(':')[-1] if any(s in b for s in ('(book)', '(series)')) else b
                                          for b in v]
-                                # aliases (elevate to main character dict)
+
+                                # aliases
                                 elif k.lower() == 'aliases':
                                     v = [e.strip() for e in v.split(',')]
 
-                                # titles (elevate to main character dict)
+                                # titles
                                 elif k.lower() == 'titles':
                                     v = [e.strip() for e in v.split(',')]
 
@@ -164,31 +144,82 @@ def process_data():
         # ignore non-cosmere characters
         cosmere_worlds = ('roshar', 'nalthis', 'scadrial', 'first of the sun', 'taldain', 'threnody', 'yolen', 'sel')
         if char_info.get('world', 'n/a').lower() not in cosmere_worlds:
-            continue
+            self._discard = True
 
-        # add to character list
-        chars.append({
-            'name': result['title'],
-            'aliases': char_info.pop('aliases', None),
-            'titles': char_info.pop('titles', None),
-            'wiki_id': result['pageid'],
-            'properties': char_info,
-            'debug_str': table_str
-        })
-
-    return chars
+        self._infobox_str = table_str
+        return char_info
 
 
-characters = process_data()
-names = set(c.get('name') for c in characters)
+def coppermind_query():
+    """load and cache data from coppermind.net"""
+
+    def _query():
+        """query coppermind.net api for all characters"""
+        # base code taken from https://www.mediawiki.org/wiki/API:Query#Continuing_queries
+        wiki_api = "https://coppermind.net/w/api.php"
+
+        # get total number of pages to fetch
+        r = requests.get(wiki_api, params=dict(action='query', format='json',
+                                               prop='categoryinfo', titles='Category:Characters'))
+        num_pages = r.json()['query']['pages']['40']['categoryinfo']['pages']
+
+        # query server until finished
+        payload = {
+            "action": "query",
+            "format": "json",
+            "prop": "revisions",
+            "generator": "categorymembers",
+            "rvprop": "content",
+            "rvsection": "0",
+            "gcmtitle": "Category:Characters",
+            "gcmprop": "ids|title",
+            "gcmtype": "page",
+            "gcmlimit": "50",
+            "formatversion": 2
+        }
+        continue_data = {}
+        with tqdm(total=num_pages, unit=' pages') as progressbar:
+            while True:
+                req = payload.copy()
+                req.update(continue_data)
+                r = requests.get(wiki_api, params=req)
+                response = r.json()
+                if 'error' in response:
+                    raise RuntimeError(response['error'])
+                if 'warnings' in response:
+                    print(response['warnings'])
+                if 'query' in response:
+                    yield response['query']['pages']
+                if 'continue' not in response:
+                    progressbar.update(len(response['query']['pages']))
+                    break
+
+                continue_data = response['continue']
+                progressbar.update(len(response['query']['pages']))
+
+    path = Path('data/characters.json')
+    if path.exists():
+        with path.open() as f:
+            return json.load(f)
+    else:
+        print('downloading from coppermind.net...')
+        results = [el for batch in _query() for el in batch]
+        with path.open('w') as f:
+            json.dump(results, f)
+        return results
+
+
+# get characters in the cosmere from coppermind.net
+characters = sorted([Character(result) for result in coppermind_query()], key=operator.attrgetter('name'))
+names = set(c.name for c in characters)
 
 
 if __name__ == '__main__':
 
     # todo: names need more sanitizing
-    print("people:", names)
-    print("duplicate names present:", len(names) != len(list(c.get('name') for c in characters)))
-    kaladin = list(c for c in characters if 'Kaladin' in c['name'])
-    print("nations:", set(c['properties'].get('nation') for c in characters))
-    print("worlds:", set(c['properties'].get('world') for c in characters))
-    print("books:", set(book for c in characters for book in c['properties'].get('books')))
+    print("people:", characters)
+    print("all character names unique:", len(names) == len(list(c.name for c in characters)))
+    print("all page ids unique:", len(set(e._pageid for e in characters)) == len(list(e._pageid for e in characters)))
+    print("nations:", set(c.info.get('nation') for c in characters))
+    print("worlds:", set(c.world for c in characters))
+    print("books:", set(book for c in characters for book in c.books))
