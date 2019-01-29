599607ce707a1c19544fa05a83b19ec6f1277f37
Zach Bernstein <zbernstein@rightpoint.com>
2019-01-29 09:04:42 -0600

    add type hints to paths

--- utils/paths.py
+++ utils/paths.py
@@ -1,20 +1,20 @@
 from pathlib import Path
 
 # key paths in project
-root_dir = Path(__file__).parent.parent
-site_dir = root_dir / 'docs'
-log_dir = root_dir / 'logs'
+root_dir: Path = Path(__file__).parent.parent
+site_dir: Path = root_dir / 'docs'
+log_dir: Path = root_dir / 'logs'
 
-data_dir = root_dir / 'data'
-cache_dir = data_dir / 'cache'
-book_dir = data_dir / 'books'
+data_dir: Path = root_dir / 'data'
+cache_dir: Path = data_dir / 'cache'
+book_dir: Path = data_dir / 'books'
 
 # cached data
-coppermind_cache_path = cache_dir / 'coppermind.json'
+coppermind_cache_path: Path = cache_dir / 'coppermind.json'
 
 # generated data
-gml_dir = data_dir / 'networks'
-json_dir = site_dir / '_data' / 'networks'
+gml_dir: Path = data_dir / 'networks'
+json_dir: Path = site_dir / '_data' / 'networks'
 
 # ensure paths exist
 for path in (root_dir, data_dir, site_dir, log_dir, gml_dir, json_dir, cache_dir):
