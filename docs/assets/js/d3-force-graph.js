c9e27ae09dc77aa7ccf83db612bf30be3a717387
Zach Bernstein <zbernstein@rightpoint.com>
2019-02-07 11:10:22 -0600

    add persistent filters to d3 graph

--- docs/assets/js/d3-force-graph.js
+++ docs/assets/js/d3-force-graph.js
@@ -9,12 +9,11 @@
 // todo: add dynamic filters to sort by world, family, profession, etc.
 
 data.then(function (data) {
-    const width = window.innerWidth, height = 760, radius = 5;
+    const width = window.innerWidth, height = window.innerHeight, radius = 5;
+    const filters = {};
 
     // get master <svg> element
     const svg = d3.select("svg#force-graph")
-        // .attr("width", width)
-        // .attr("height", height)
         .attr("viewBox", [-width/2, -height/2, width, height].join(" "));
 
 
@@ -32,7 +31,80 @@ data.then(function (data) {
     // define colors
     const color = d3.scaleOrdinal(d3.schemeCategory10);
 
-    // define drag functions
+    // create enclosing element for graph data
+    const graph = svg.append("g")
+        .attr("class", "graph");
+
+    // enable zoom/pan on graph element
+    const zoom = d3.zoom()
+        .scaleExtent([0.25, 5])
+        .on("zoom", () => graph.attr("transform", d3.event.transform));
+    svg.call(zoom);
+
+    let link = graph.append("g").attr("class", "links").selectAll("line");
+    let node = graph.append("g").attr("class", "nodes").selectAll("circle");
+    let label = graph.append("g").attr("class", "labels").selectAll("text");
+
+    // initial drawing with all data
+    update(data);
+
+    // add legend
+    const legend = svg.append("g")
+        .attr("class", "legend")
+        .selectAll(".series")
+        .data(color.domain())
+        .enter()
+        .append("g")
+        .attr("class", "series")
+        .attr("transform", (d, i) => `translate(0, ${20 * i})`)
+        .style("cursor", "pointer")
+        .on("click", datum => { // apply filter on click
+            toggleFilter("world", datum);
+            update(subset(data, filters));
+        });
+
+    legend.append("circle")
+        .attr("r", ".3em")
+        .attr("cx", width/2 - (0.025 * width))
+        .attr("cy", -height/2 + 50)
+        .style("fill", color);
+        // todo: format circle as more transparent if series not displayed
+
+    legend.append("text")
+        .attr("x", width/2 - (0.025 * width) - 25)
+        .attr("y", -height/2 + 50 + 1)
+        .attr("dx", 12)
+        .style("font-size", ".6em")
+        .style("text-anchor", "end")
+        .style("alignment-baseline", "middle")
+        .text(d => d);
+        // todo: format text differently if series is displayed
+
+    const legendTitle = svg.select(".legend")
+        .insert("text", ":first-child")
+        .attr("x", width/2 - (0.025 * width) + radius)
+        .attr("y", -height/2 + 50 + 1)
+        .attr("transform", "translate(0, -20)")
+        .style("font-size", ".8em")
+        .style("text-anchor", "end")
+        .text("World");
+
+    function tick() {
+        label
+            .attr("transform", d => `translate(${d.x}, ${d.y})`)
+            .attr("cx", d => d.x)
+            .attr("cy", d => d.y);
+
+        node
+            .attr("cx", d => d.x)
+            .attr("cy", d => d.y);
+
+        link
+            .attr("x1", d => d.source.x)
+            .attr("y1", d => d.source.y)
+            .attr("x2", d => d.target.x)
+            .attr("y2", d => d.target.y);
+    }
     function drag(simulation) {
 
         // gentlemen, start your engines!
@@ -62,53 +134,45 @@ data.then(function (data) {
             .on("drag", dragging)
             .on("end", dragEnd);
     }
+    function subset(data, filters) {
+        const nodes = data.nodes
+            .filter(n => Object.keys(n)
+                .some(k => {
+                    if (filters.hasOwnProperty(k))
+                        return filters[k].has(n[k]);
+                    else return false;
+                }));
+        const links = data.links.filter(l => nodes.includes(l.source) && nodes.includes(l.target));
 
-    // create enclosing element for graph data
-    const graph = svg.append("g")
-        .attr("class", "graph");
-
-    // enable zoom/pan on graph element
-    const zoom = d3.zoom()
-        .scaleExtent([0.25, 5])
-        .on("zoom", () => graph.attr("transform", d3.event.transform));
-    svg.call(zoom);
-
-
-    // add links between nodes first
-    let link = graph.append("g")
-        .attr("class", "links")
-        .selectAll("line");
-
-    // add nodes on top
-    let node = graph.append("g")
-        .attr("class", "nodes")
-        .selectAll("circle");
-
-    // add labels
-    let label = graph.append("g")
-        .attr("class", "labels")
-        .selectAll("text");
-
-    function filterSubset(key = null, value = null) {
-        const graph = {};
-
-        if (key == null || value == null) {
-            // populate graph object with all data points
-            graph.nodes = data.nodes.filter(() => true);
-            graph.links = data.links.filter(() => true);
-        } else {
-            // populate graph object with nodes and links that satisfy filter
-            graph.nodes = data.nodes.filter(n => n[key] === value);
-            graph.links = data.links.filter(l => graph.nodes.includes(l.source) && graph.nodes.includes(l.target));
-        }
+        return {
+            nodes,
+            links
+        };
+    }
+    function explicitSubset(data, key, value) {
+        const graph = {
+            nodes: data.nodes.filter(n => n[key] === value),
+            links: data.links.filter(l => graph.nodes.includes(l.source) && graph.nodes.includes(l.target))
+        };
 
         return graph;
     }
-    function update(subset) {
+    function toggleFilter(key, value) {
+        // make sure that key is represented in filters
+        if (!filters.hasOwnProperty(key))
+            filters[key] = new Set();
+
+        // toggle filter
+        if (filters[key].has(value))
+            filters[key].delete(value);
+        else
+            filters[key].add(value);
+    }
+    function update(data) {
         // update
-        link = link.data(subset.links, d => d.index);
-        node = node.data(subset.nodes, d => d.id);
-        label = label.data(subset.nodes, d => d.id);
+        link = link.data(data.links, d => d.index);
+        node = node.data(data.nodes, d => d.id);
+        label = label.data(data.nodes, d => d.id);
 
         // exit
         link.exit().remove();
@@ -148,67 +212,11 @@ data.then(function (data) {
 
         // restart simulation
         simulation.on("tick", tick)
-            .nodes(subset.nodes)
-            .force("links", d3.forceLink(subset.links).id(d => d.id).distance(50))
+            .nodes(data.nodes)
+            .force("links", d3.forceLink(data.links).id(d => d.id).distance(50))
             .alpha(1)
             .alphaTarget(0)
             .restart();
-    }
-
-    // initial drawing with all data
-    update(data);
-
-    // add legend
-    const legend = svg.append("g")
-        .attr("class", "legend")
-        .selectAll(".series")
-        .data(color.domain())
-        .enter()
-        .append("g")
-        .attr("class", "series")
-        .attr("transform", (d, i) => `translate(0, ${20 * i})`)
-        .style("cursor", "pointer")
-        .on("click", datum => update(filterSubset("world", datum))); // apply filter on click
-
-    legend.append("circle")
-        .attr("r", ".3em")
-        .attr("cx", width/2 - (0.025 * width))
-        .attr("cy", -height/2 + 50)
-        .style("fill", color);
-
-    legend.append("text")
-        .attr("x", width/2 - (0.025 * width) - 25)
-        .attr("y", -height/2 + 50 + 1)
-        .attr("dx", 12)
-        .style("font-size", ".6em")
-        .style("text-anchor", "end")
-        .style("alignment-baseline", "middle")
-        .text(d => d);
-
-    const legendTitle = svg.select(".legend")
-        .insert("text", ":first-child")
-        .attr("x", width/2 - (0.025 * width) + radius)
-        .attr("y", -height/2 + 50 + 1)
-        .attr("transform", "translate(0, -20)")
-        .style("font-size", ".8em")
-        .style("text-anchor", "end")
-        .text("World");
-
-    function tick() {
-        label
-            .attr("transform", d => `translate(${d.x}, ${d.y})`)
-            .attr("cx", d => d.x)
-            .attr("cy", d => d.y);
-
-        node
-            .attr("cx", d => d.x)
-            .attr("cy", d => d.y);
-
-        link
-            .attr("x1", d => d.source.x)
-            .attr("y1", d => d.source.y)
-            .attr("x2", d => d.target.x)
-            .attr("y2", d => d.target.y);
     }
 });
 
