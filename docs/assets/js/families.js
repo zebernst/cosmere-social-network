f4f1f47d7fb0422cde8c5adcc5e38ca76d03db90
Zach Bernstein <zbernstein@rightpoint.com>
2019-02-11 15:17:11 -0600

    implement d3 reusable pattern to avoid polluting global namespace

--- docs/assets/js/families.js
+++ docs/assets/js/families.js
@@ -1,52 +1,15 @@
 /* d3 force graph for family relationships *
  *    [setup located in d3-force.js]       */
 
-// apply data
+let graph = forceDirectedGraph();
+
 data.then(function (data) {
 
+    let svg = d3.select('svg#force-graph').datum(data);
+
     // populate color space
-    color.domain([...new Set(data.nodes.map(n => n.world))]);
+    graph = graph
+        .colorDomain([...new Set(data.nodes.map(n => n.world))]);
 
-    // initial drawing with all data
-    // (transforms links into node references)
-    update(data);
-
-    // update nodes with neighbor array (needs to be called after initial update())
-    populateConnections(data);
-
-    // add legend
-    legend = legend
-        .data(color.domain())
-        .enter()
-        .append("g")
-        .attr("class", "series")
-        .attr("transform", (d, i) => `translate(0, ${20 * i})`)
-        .on("click", datum => { // apply filter on click
-            toggleFilter("world", datum);
-            update(subset(data, filters));
-        });
-
-    legend
-        .append("circle")
-        .attr("r", ".3em")
-        .attr("cx", width/2 - (0.025 * width))
-        .attr("cy", -height/2 + 50)
-        .attr("opacity", 0.4) // display filter as disabled initially
-        .style("fill", color);
-
-    legend
-        .append("text")
-        .attr("x", width/2 - (0.025 * width) - 25)
-        .attr("y", -height/2 + 50 + 1)
-        .attr("dx", 12)
-        .style("font-weight", "normal") // display filter as disabled initially
-        .text(d => d);
-
-    // add legend title
-    legendTitle
-        .attr("class", "legend-title")
-        .attr("x", width/2 - (0.025 * width) + radius)
-        .attr("y", -height/2 + 50 + 1)
-        .attr("transform", "translate(0, -20)")
-        .text("World");
+    svg.call(graph);
 });
