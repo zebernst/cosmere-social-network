6552271ced3e240b151c7483ee85a826f324dc78
Zach Bernstein <zbernstein@rightpoint.com>
2019-02-12 13:26:43 -0600

    reorganize js includes in project
    - switch to cdn for d3.js
    - add underscore.js
    - move d3.js include from head to footer
    - add reference link to mike bostock's reusable chart pattern
    - move d3.json call from families.html to families.js

--- docs/assets/js/families.js
+++ docs/assets/js/families.js
@@ -4,29 +4,30 @@
 // persist graph
 let graph;
 
-data.then(function (data) {
-    let svg = d3.select('svg#force-graph')
-        .datum(data);
+const data = d3.json(dataUrl)
+    .then(function (data) {
+        let svg = d3.select('svg#force-graph')
+            .datum(data);
 
-    let legend = [
-        {color: "#1f77b4", world: "Sel"},
-        {color: "#ff7f0e", world: "Roshar"},
-        {color: "#2ca02c", world: "Scadrial"},
-        {color: "#d62728", world: "Nalthis"},
-        {color: "#9467bd", world: "Threnody"},
-        {color: "#8c564b", world: "First of the Sun"},
-        {color: "#e377c2", world: "Taldain"}
-        ];
+        let legend = [
+            {color: "#1f77b4", world: "Sel"},
+            {color: "#ff7f0e", world: "Roshar"},
+            {color: "#2ca02c", world: "Scadrial"},
+            {color: "#d62728", world: "Nalthis"},
+            {color: "#9467bd", world: "Threnody"},
+            {color: "#8c564b", world: "First of the Sun"},
+            {color: "#e377c2", world: "Taldain"}
+            ];
 
-    // create graph
-    graph = forceDirectedGraph()
-        .width(window.innerWidth)
-        .height(window.innerHeight)
-        .radius(5)
-        .legendKey('world')
-        .colorDomain(legend.map(e => e.world))
-        .colorRange(legend.map(e => e.color));
+        // create graph
+        graph = forceDirectedGraph()
+            .width(window.innerWidth)
+            .height(window.innerHeight)
+            .radius(5)
+            .legendKey('world')
+            .colorDomain(legend.map(e => e.world))
+            .colorRange(legend.map(e => e.color));
 
-    // bind graph to svg
-    svg.call(graph);
-});
+        // bind graph to svg
+        svg.call(graph);
+    });
