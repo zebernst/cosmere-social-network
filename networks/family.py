6e029a6e5b4a7ff38ff21748feacdad06be34921
Zach Bernstein <zebernst@gmail.com>
2019-04-07 21:55:42 +1200

    make family network code more readable

--- networks/family.py
+++ networks/family.py
@@ -36,59 +36,60 @@ def create_graph() -> nx.OrderedGraph:
     logger.debug("Added character nodes to graph.")
 
     # add edges between relevant nodes
-    for c in characters:
+    for char in characters:
         # ignore characters with no family info
-        if c.name not in nodes:
+        if char.name not in nodes:
             continue
 
         # loop through character's family connections
-        for cxns in (c.info[k] for k in fields if k in c.info):
+        for cxns in (char.info[k] for k in fields if k in char.info):
             for link in mwp.parse(cxns).filter_wikilinks():
                 # sanitize
                 relation = link.title.strip_code()
-                if '_' in relation:
+                if "_" in relation:
                     relation = relation.replace('_', ' ')
                 if "'s " in relation:
                     continue
 
                 # get potential full name
-                forename = relation.split(' ')[0]
-                surname = c.name.split(' ')[-1]
+                relation_forename = relation.split(' ')[0]
+                char_surname = char.name.split(' ')[-1]
 
-                target = None
+                target_id = None
 
                 # if direct match found, add edge to graph
                 if relation in nodes:
-                    target = relation
-                    logger.debug(f'Identified exact target of edge ({c.name}, {relation}).')
+                    target_id = relation
+                    logger.debug(f'Identified exact target of edge ({char.name}, {relation}).')
 
                 # check if characters share a surname
-                elif f"{forename} {surname}" in nodes:
-                    target = f"{forename} {surname}"
-                    logger.debug(f'Identified likely target of edge ({c.name}, {relation}) '
-                                 f'to be {forename} {surname} via surname.')
+                elif f"{relation_forename} {char_surname}" in nodes:
+                    target_id = f"{relation_forename} {char_surname}"
+                    logger.debug(f'Identified likely target of edge ({char.name}, {relation}) '
+                                 f'to be {relation_forename} {char_surname} via surname.')
 
                 # check if connection matches any character aliases
                 elif relation in monikers:
-                    target = monikers[relation].name
-                    logger.debug(f'Identified likely target of edge ({c.name}, {relation}) '
+                    target_id = monikers[relation].name
+                    logger.debug(f'Identified likely target of edge ({char.name}, {relation}) '
                                  f'to be {monikers[relation].name} via alias.')
 
                 # last resort: loop through all characters and match substrings
                 else:
                     found = False
-                    for name, char in monikers.items():
+                    for name, other in monikers.items():
                         if relation in name:
-                            target = char.name
+                            target_id = other.name
                             found = True
-                            logger.debug(f'Could not confirm target of edge ({c.name}, {relation}), assuming {name}.')
+                            logger.debug(f'Could not confirm target of edge ({char.name}, {relation}), '
+                                         f'assuming {target_id}.')
                             break
                     if not found:
-                        logger.info(f'Could not identify target of edge ({c.name}, {relation}).')
+                        logger.info(f'Could not identify target of edge ({char.name}, {relation}).')
 
                 # add edge if target is valid
-                if target and target in nodes:
-                    G.add_edge(c.name, target)
+                if target_id and target_id in nodes:
+                    G.add_edge(char.name, target_id)
 
     return G
 
