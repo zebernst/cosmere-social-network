83b6637c112fc7f05ceab6e7b71f487fc18e692a
Zach Bernstein <zbernstein@rightpoint.com>
2019-07-11 11:41:50 -0500

    Divorce name detection and disambiguation into two separate files

--- core/disambiguation.py
+++ core/disambiguation.py
@@ -1,176 +1,121 @@
-import yaml
+from typing import Optional
 
 from core.characters import Character, characters_
-from core.constants import book_keys, titles, worlds
-from utils.epub import chapters
+from utils.input import ask, menu, yn_question
 from utils.logs import create_logger
-from utils.paths import disambiguation_dir
 
 
 # todo: add logging
 logger = create_logger('csn.core.disambiguation')
 
+_characters = list(characters_)
+_char_ids = {c.id: c for c in _characters}
+monikers = {}
+for c in _characters:
+    for one in c.monikers:
+        if one in monikers:
+            if isinstance(monikers[one], Character):
+                monikers[one] = [monikers[one], c]
+            elif isinstance(monikers[one], list):
+                monikers[one].append(c)
+        else:
+            monikers[one] = c
 
-# todo: split name identification in runs into separate file, divorce from disambiguation
 
-def clarify_REFACTORME(name: str, names: dict, line: str):
-    # todo: check character's .books attribute against current processing key
+def char_search(prompt: Optional[str]) -> Optional[Character]:
+    response = ask(prompt=prompt,
+                   validator=lambda r: r in monikers,
+                   error="Character not found.")
 
-    local_chars = [c for c in names[name] if any(b == key.split('/')[0] for b in c.books)]
+    if response is None:
+        exit(1)
+
+    if isinstance(monikers[response], list):
+        print(f"Multiple matches found for {response}. Please specify:")
+        for c in monikers[response]:
+            print(f"  {c.name} ({c.world}) -- {c.info}")
+        return char_search(prompt=None)
+    else:
+        return monikers[response]
+
+
+def clarify_list(key: str, name: str, names: dict):
+
+    local_chars = [c for c in names[name] if verify_presence(key, c, name)]
+    if not local_chars:
+        return None
+
     if len(local_chars) == 1:
-        logger.debug(f'Automatically matched ambiguous character {repr(local_chars[0])} from {name} -- '
-                     f'only character with presence in {key}')
+        logger.debug(f'Match of ambiguous character confirmed automatically -- "{name}" identified as '
+                     f'{repr(local_chars[0])} with presence in {key}.')
         return local_chars[0]
 
-    print(f'Ambiguous reference found for `{name}`! Please choose the correct character that '
-          f'appears in the following run:')
-    print(line)
-    for i2, name2 in enumerate(names[name]):
-        print(f"  {i2 + 1}: {name2.name} ({name2.world}) -- {name2.info}")
-    print("  o: The character is not listed.")
-    print("  x: This is not a character.")
-    response = input("> ")
-    while not ((response.isdigit() and int(response) <= len(names[name]))
-               or response.lower().startswith('x')
-               or response.lower().startswith('o')):
-        response = input("> ")
+    response = menu(prompt=f'Ambiguous reference found for "{name}"! Please choose the correct character.',
+                    options=[f"  {i+1}: {c.name} ({c.world}) -- {c.info}" for i, c in enumerate(local_chars)]
+                          + [f"  o: The correct character is not listed.",
+                             f"  x: This is not a character."],
+                    validator=lambda r: (r.isdigit() and int(r) <= len(local_chars))
+                                        or r.lower().startswith('x')
+                                        or r.lower().startswith('o'))
+
     if response.startswith('x'):
         return None
-    if response.startswith('o'):
-        response = input("Type the name of the character the keyword is referring to: ")
-        while response not in names:
-            response = input("Character not found. Try again: ")
-
-        if isinstance(names[response], list):
-            # todo: clean up recursive calls
-            ch = clarify_REFACTORME(response, names, line)
-        else:
-            ch = names[response]
+    elif response.startswith('o'):
+        ch = char_search("Type the name of the character the keyword is referring to: ")
+        logger.debug(f'Match of ambiguous character confirmed manually      -- "{name}" identified as {repr(ch)}.')
+        return ch
     else:
-        idx2 = int(response) - 1
-        ch = names[name][idx2]
-
-    logger.debug(f'Manually matched ambiguous character {repr(ch)}.')
-    return ch
+        ch = names[name][int(response) - 1]
+        logger.debug(f'Match of ambiguous character confirmed from list     -- "{name}" identified as {repr(ch)}.')
+        return ch
 
 
-def verify(key: str, ch: Character, word: str):
-    belongs = any(b == key.split('/')[0] for b in ch.books)
-    if not belongs:
-        logger.debug(f'Ignoring potential match of {repr(ch)} from "{word}" - character does not appear in {key}')
-    return belongs
+def verify_presence(key: str, ch: Character, word: str):
+    in_book = any(b == key.split('/')[0] for b in ch.books)
+    if not in_book:
+        logger.debug(f'Match of ambiguous character rejected  automatically -- "{word}" identified as '
+                     f'{repr(ch)} does not appear in {key}.')
+    return in_book
 
 
-if __name__ == '__main__':
-    characters = list(characters_)
+def save(pos: int, char: Optional[Character], disambiguation: dict) -> None:
+    disambiguation[pos] = char.id if char is not None else None
 
-    monikers = {}
-    for c in characters:
-        for name in c.monikers:
-            if name in monikers:
-                if isinstance(monikers[name], Character):
-                    monikers[name] = [monikers[name], c]
-                elif isinstance(monikers[name], list):
-                    monikers[name].append(c)
-            else:
-                monikers[name] = c
 
-    char_ids = {c.id: c for c in characters}
+def recall(pos: int, disambiguation: dict) -> Optional[Character]:
+    return _char_ids.get(disambiguation.get(pos), None)
 
-    for key in book_keys:
-        if key != 'mistborn/era2/bands-of-mourning':
-            continue
 
-        world = worlds.get(key)
+def disambiguate_name(key: str, name: str, disambiguation: dict, pos: int) -> Optional[Character]:
+    if pos in disambiguation:
+        char = recall(pos, disambiguation)
+        if char is not None:
+            logger.debug(f'Match of ambiguous character confirmed automatically -- '
+                         f'{repr(char)} found in disambiguation.')
+        return char
 
-        with (disambiguation_dir / key).with_suffix('.yml').open(mode='r') as f:
-            disambiguation = yaml.load(f, yaml.Loader)
-            if disambiguation is None:
-                disambiguation = {}
+    else:
+        char = clarify_list(key, name, monikers)
+        save(pos, char, disambiguation)
+        return char
 
-        for chapter, tokens in chapters(key):
-            run_size = 25
-            idx = 0
 
-            if chapter not in disambiguation:
-                disambiguation[chapter] = {}
+def disambiguate_title(title: str, disambiguation: dict, pos: int) -> Optional[Character]:
+    if pos in disambiguation:
+        char = recall(pos, disambiguation)
+        if char is not None:
+            logger.debug(f'Match of ambiguous character confirmed automatically -- '
+                         f'{repr(char)} found in disambiguation.')
+        return char
 
-            while idx < len(tokens):
-                local_tokens = tokens[idx:idx + run_size]
-                run = ' '.join(local_tokens).strip()
-
-                chars = []
-                i = 0
-                while i < len(local_tokens):
-                    name = local_tokens[i]
-                    full_name = ' '.join(local_tokens[i:i + 2])
-                    char: Character = None
-
-                    if full_name in monikers:
-                        if isinstance(monikers[full_name], list):
-                            if idx + i in disambiguation[chapter]:
-                                char_id = disambiguation[chapter][idx + i]
-                                char = char_ids[char_id] if char_id is not None else None
-                            else:
-                                char = clarify_REFACTORME(full_name, monikers, run)
-                                disambiguation[chapter][idx + i] = char.id if char else None
-                        else:
-                            char = monikers[full_name] if verify(key, monikers[full_name], full_name) else None
-                        i += 2
-
-                    elif full_name not in monikers and name in titles:
-                        i += 1
-                        continue
-
-                    elif name in monikers:
-                        if isinstance(monikers[name], list):
-                            if idx + i in disambiguation[chapter]:
-                                char_id = disambiguation[chapter][idx + i]
-                                char = char_ids[char_id] if char_id is not None else None
-                            else:
-                                char = clarify_REFACTORME(name, monikers, run)
-                                disambiguation[chapter][idx + i] = char.id if char else None
-                        else:
-                            char = monikers[name] if verify(key, monikers[name], name) else None
-                        i += 1
-
-                    else:
-                        i += 1
-                        continue
-
-                    # todo: advance by two tokens if possible, else fall back to one
-
-                    # todo: disambiguation stores word, resolved character, and position, checks if resolved
-                    #       word exists within length of last run
-
-                    if char is not None:
-                        # if not verify(key, char):
-                        #     char = None
-                        if char.world.lower() != world and 'worldhopping' not in char.abilities:
-                            if idx + i not in disambiguation[chapter]:
-                                print(f"Non-native non-worldhopping character found! Please confirm presence "
-                                      f"of {char.name} (from {char.world}) in the following run:")
-                                print(run)
-                                disambiguation[chapter][idx + i] = input(">  character present? y/n: ").lower().startswith('y')
-                            elif disambiguation[chapter][idx + i]:
-                                chars.append((i, char))
-                        else:
-                            chars.append((i, char))
-
-                # advance past first found character
-                if len(chars) >= 2:
-                    idx += chars[0][0] + 1
-                    print(f"{run:200s} // {chars}")
-
-                # advance until only character is at beginning of run (max threshold)
-                elif len(chars) == 1:
-                    idx += chars[0][0] if chars[0][0] > 0 else 1
-                    print(f"{run:200s} // {chars}")
-
-                # skip run if no chars found
-                else:
-                    idx += run_size - 1
-                    print(f"{run:200s} // {chars}")
-
-            with (disambiguation_dir / key).with_suffix('.yml').open(mode='w') as f:
-                yaml.dump(disambiguation, f, yaml.Dumper, default_flow_style=False, sort_keys=False)
+    else:
+        if yn_question(f'Ambiguous reference found for "{title}". '
+                       f'Does this refer to a character?'):
+            char = char_search(f"Who does this refer to?")
+            save(pos, char, disambiguation)
+        else:
+            char = None
+            save(pos, None, disambiguation)
+        if char is not None:
+            logger.debug(f'Match of ambiguous character confirmed manually      -- "{title}" identified as {repr(char)}.')
+        return char
--- networks/interactions.py
+++ networks/interactions.py
@@ -24,3 +24,113 @@
 # make this file (interactions.py) rely on a saved datastructure or use a dispatchable method to fetch
 # the data, to enable divorcing of analysis from source material and allowing user to specify which
 # kind they have.
+import yaml
+
+from core.characters import Character
+from core.constants import book_keys, worlds, titles
+from core.disambiguation import disambiguate_name, verify_presence, monikers, char_search, save, disambiguate_title
+from utils.epub import chapters
+from utils.input import yn_question
+from utils.paths import disambiguation_dir
+
+
+for book in book_keys:
+    if book != 'mistborn/era2/bands-of-mourning':
+        continue
+
+    world = worlds.get(book)
+
+    with (disambiguation_dir / book).with_suffix('.yml').open(mode='r') as f:
+        disambiguation = yaml.load(f, yaml.Loader)
+        if disambiguation is None:
+            disambiguation = {}
+
+    for chapter, tokens in chapters(book):
+        run_size = 25
+        idx = 0
+
+        if chapter not in disambiguation:
+            disambiguation[chapter] = {}
+
+        while idx < len(tokens):
+            local_tokens = tokens[idx:idx + run_size]
+            run = ' '.join(local_tokens).strip()
+
+            chars = []
+            i = 0
+            while i < len(local_tokens):
+                this_token = local_tokens[i]
+                next_token = local_tokens[i+1] if i+1 < len(local_tokens) else ''
+                third_token = local_tokens[i + 2] if i + 2 < len(local_tokens) else ''
+                two_tokens = this_token + ' ' + next_token
+                next_two_tokens = next_token + ' ' + third_token
+                three_tokens = two_tokens + ' ' + third_token
+
+                char: Character = None
+
+                if three_tokens in monikers:
+                    if isinstance(monikers[three_tokens], list):
+                        print(run)
+                        char = disambiguate_name(book, three_tokens, disambiguation[chapter], pos=idx + i)
+                    else:
+                        char = monikers[three_tokens] if verify_presence(book, monikers[three_tokens], three_tokens) else None
+
+                    i += 3
+
+                elif two_tokens in monikers:
+                    if isinstance(monikers[two_tokens], list):
+                        print(run)
+                        char = disambiguate_name(book, two_tokens, disambiguation[chapter], pos=idx + i)
+                    else:
+                        char = monikers[two_tokens] if verify_presence(book, monikers[two_tokens], two_tokens) else None
+
+                    i += 2
+
+                elif this_token in titles:
+                    if next_token not in monikers and next_two_tokens not in monikers:
+                        print(run)
+                        char = disambiguate_title(this_token, disambiguation[chapter], pos=idx + i)
+                    i += 1
+
+                elif this_token in monikers:
+                    if isinstance(monikers[this_token], list):
+                        print(run)
+                        char = disambiguate_name(book, this_token, disambiguation[chapter], pos=idx + i)
+                    else:
+                        char = monikers[this_token] if verify_presence(book, monikers[this_token], this_token) else None
+
+                    i += 1
+
+                else:
+                    i += 1
+                    continue
+
+                if char is not None:
+                    if char.world.lower() != world and 'worldhopping' not in char.abilities:
+                        if idx + i not in disambiguation[chapter]:
+                            print(f"Non-native non-worldhopping character found! Please confirm presence "
+                                  f"of {char.name} (from {char.world}) in the following run (y/n):")
+                            print(run)
+                            disambiguation[chapter][idx + i] = input("> ").lower().startswith('y')
+                        elif disambiguation[chapter][idx + i]:
+                            chars.append((i, char))
+                    else:
+                        chars.append((i, char))
+
+            # advance past first found character
+            if len(chars) >= 2:
+                idx += chars[0][0] + 1
+                print(f"{run:200s} // {chars}")
+
+            # advance until only character is at beginning of run (max threshold)
+            elif len(chars) == 1:
+                idx += chars[0][0] if chars[0][0] > 0 else 1
+                print(f"{run:200s} // {chars}")
+
+            # skip run if no chars found
+            else:
+                idx += run_size - 1
+                print(f"{run:200s} // {chars}")
+
+        with (disambiguation_dir / book).with_suffix('.yml').open(mode='w') as f:
+            yaml.dump(disambiguation, f, yaml.Dumper, default_flow_style=False, sort_keys=False)
