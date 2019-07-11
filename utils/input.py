daaad7478bf4b04260828377a4e9463e423d9c13
Zach Bernstein <zbernstein@rightpoint.com>
2019-07-11 08:42:02 -0500

    Add optional error message to user-input functions

--- utils/input.py
+++ utils/input.py
@@ -1,21 +1,25 @@
 from typing import Callable
 
 
-def menu(prompt: str, options: list, validator: Callable[[str], bool]):
+def menu(prompt: str, options: list, validator: Callable[[str], bool], error: str = None):
     print(prompt)
     for opt in options:
         print(opt)
     response = input("> ")
     while not validator(response):
-        input("> ")
+        if error:
+            print(error)
+        response = input("> ")
     return response
 
 
-def ask(prompt: str, validation: Callable[[str], bool]):
+def ask(prompt: str, validator: Callable[[str], bool], error: str = None):
     print(prompt)
     response = input("> ")
-    while not validation(response):
-        input("> ")
+    while not validator(response):
+        if error:
+            print(error)
+        response = input("> ")
     return response
 
 
@@ -23,5 +27,5 @@ def yn_question(prompt: str):
     print(prompt + " (y/n)")
     response = input("> ")
     while not any(response.lower().startswith(s) for s in 'yn'):
-        input("> ")
+        response = input("> ")
     return response.startswith('y')
