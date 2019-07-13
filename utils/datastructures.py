eaee30c3735740f6946e0fef49802c18acf2c649
Zach Bernstein <zbernstein@rightpoint.com>
2019-07-12 21:13:17 -0500

    Create basic framework for trie

--- utils/datastructures.py
+++ utils/datastructures.py
@@ -1,4 +1,32 @@
+from collections import Iterable
+from typing import Any
+
+
+class _TrieNode:
+    def __init__(self, key: str, data: Any = None):
+        self.key = key
+        self.children = []
+        if isinstance(data, Iterable):
+            self.data = list(data)
+        elif data is not None:
+            self.data = [data]
+        else:
+            self.data = []
+
+    @property
+    def has_data(self):
+        return self.data
 
 
 class Trie:
-    pass
+    # todo: implement len/getitem/setitem/delitem/contains/iter and slicing (w/ start only) a la pygtrie
+    def __init__(self):
+        self.root = _TrieNode('')
+
+    def __setitem__(self, key: str, value: Any):
+        if not key:
+            pass  # todo
+
+        node = self.root
+        for char in key:
+            pass
